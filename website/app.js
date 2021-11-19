d5a1e19edfe83b5fe99a6879ab821b9b7caf30ff
George Kent-Scheller <georgeks@bu.edu>
2021-11-19 13:54:22 -0500

    added a comments so my ide can see the branch

--- website/app.js
+++ website/app.js
@@ -21,7 +21,7 @@ app.listen(3000, () => {
 });
 
 app.use(express.static(__dirname));
-
+//think this maybe incorrect paridime
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
   res.sendFile(__dirname + "/gotcha.html");
